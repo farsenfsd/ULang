24513f0a01916128c64183ccf4331e78b776d8ee
Luís MIguel <farsenfsd@gmail.com>
2022-06-16 17:11:35 +0100

    Avanços interface professor

--- app/src/main/java/com/example/projectoubi_41400/prof_criar.java
+++ app/src/main/java/com/example/projectoubi_41400/prof_criar.java
@@ -1,7 +1,15 @@
 package com.example.projectoubi_41400;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -9,9 +17,71 @@ import com.google.firebase.firestore.FirebaseFirestore;
 
 public class prof_criar extends AppCompatActivity {
 
+    Button add;
+    AlertDialog dialog;
+    LinearLayout layout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prof_criar);
+
+        add = findViewById(R.id.addCategoria);
+        layout = findViewById(R.id.container);
+
+        buildDialog();
+
     }
+
+    public void OpenAbrirDialogo(View v) {
+        dialog.show();
+    }
+
+    public void OpenGuardarPacote(View v) {
+        // Guardar o pacote
+    }
+
+    private void buildDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        View view = getLayoutInflater().inflate(R.layout.dialog,null);
+
+        EditText name = view.findViewById(R.id.catEdit);
+
+        builder.setView(view);
+        builder.setTitle("Introduza o nome da categoria")
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        addCard(name.getText().toString());
+                    }
+                })
+                .setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+        dialog = builder.create();
+    }
+
+    private void addCard(String name) {
+        View view = getLayoutInflater().inflate(R.layout.card, null);
+
+        TextView nameView = view.findViewById(R.id.name);
+        Button delete = view.findViewById(R.id.deleteCat);
+
+        nameView.setText(name);
+
+
+        delete.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                layout.removeView(view);
+            }
+        });
+
+        layout.addView(view);
+
+    }
+
 }
