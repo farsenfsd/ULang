ef3f8e88e964cca3b221e7b0dcd1814d1f722a56
Luís MIguel <farsenfsd@gmail.com>
2022-06-22 05:54:35 +0100

    Started work on PackageManager

--- app/src/main/java/com/example/projectoubi_41400/gerirPacotes.java
+++ app/src/main/java/com/example/projectoubi_41400/gerirPacotes.java
@@ -1,4 +1,29 @@
 package com.example.projectoubi_41400;
 
-public class gerirPacotes {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class gerirPacotes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gerir_linguas);
+    }
+
+    public void Leave(View v){
+        endActivity(v);
+    }
+
+    public void endActivity ( View v) {
+        Intent intent = new Intent();
+        setResult(2,intent);
+
+        finish();
+    }
 }
